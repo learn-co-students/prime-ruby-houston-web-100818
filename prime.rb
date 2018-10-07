17de8abd173f918334f029d7814f48eb12928ea3
Alexander Carlin <ahcarlin@gmail.com>
2018-10-07 00:17:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false 
+  end
+  
+  range_array =* (1..num)
+  count = 0
+  range_array.each do |i|
+    if num % i == 0 
+      count += 1 
+    end
+  end
+  if count > 2 
+    return false 
+  else 
+    return true
+  end
+end 
